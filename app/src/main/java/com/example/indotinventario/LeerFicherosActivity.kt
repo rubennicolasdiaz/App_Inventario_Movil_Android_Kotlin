f892c622d32cc35cc453c39e1962ff18251d5a9d
RubenNicolas85 <117572476+RubenNicolas85@users.noreply.github.com>
2024-12-03 21:39:15 +0100

    Update LeerFicherosActivity.kt
    Perfeccionamiento de la lectura de ficheros

--- app/src/main/java/com/example/indotinventario/LeerFicherosActivity.kt
+++ app/src/main/java/com/example/indotinventario/LeerFicherosActivity.kt
@@ -1,29 +1,16 @@
 package com.example.indotinventario
 
-
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.indotinventario.databinding.ActivityLeerFicherosBinding
-import android.content.ContentResolver
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Environment
 import android.provider.DocumentsContract
-import android.provider.MediaStore
 import android.util.Log
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
-import androidx.core.os.EnvironmentCompat
-import java.io.BufferedReader
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import java.io.InputStreamReader
 
 class LeerFicherosActivity : AppCompatActivity() {
 
@@ -41,6 +28,7 @@ class LeerFicherosActivity : AppCompatActivity() {
 
     private fun cargarVista() {
         binding.buttonLeerArchivos.setOnClickListener {
+
             val uri = Uri.parse(Environment.DIRECTORY_DCIM)
             openFile(uri)
         }
@@ -59,65 +47,52 @@ class LeerFicherosActivity : AppCompatActivity() {
         startActivityForResult(intent, PICK_JSON_FILE)
     }
 
-    /*
     // Este método manejará el resultado del archivo seleccionado
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        var targetFile: File? = null
         if (requestCode == PICK_JSON_FILE && resultCode == RESULT_OK) {
             val uri = data?.data ?: return
 
-            // Copiar el archivo a un directorio accesible
-            val targetFile = File(filesDir, "pepe.json")
-            copyFile(uri, targetFile)
-        }
+            if(uri.toString().contains("partidas", ignoreCase = true)){
 
+                // Definir el archivo de destino
+                targetFile = File((this@LeerFicherosActivity.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)), "partidas.json")
 
-    }
-
-    private fun copyFile(uri: Uri, destinationFile: File) {
-        try {
-            contentResolver.openInputStream(uri)?.use { inputStream ->
-                FileOutputStream(destinationFile).use { outputStream ->
-                    val buffer = ByteArray(1024)
-                    var length: Int
-                    while (inputStream.read(buffer).also { length = it } > 0) {
-                        outputStream.write(buffer, 0, length)
-                    }
+                // Verificar si el archivo ya existe
+                if (targetFile.exists()) {
+                    // Si el archivo existe, eliminarlo para sobrescribirlo
+                    targetFile.delete()
+                    Log.d("FileCheck", "El archivo ${targetFile.name} ya existía, se ha eliminado para sobrescribirlo.")
                 }
+                copyFile(uri, targetFile)
+            }else if(uri.toString().contains("articulos", ignoreCase = true)){
+
+                // Definir el archivo de destino
+                targetFile = File((this@LeerFicherosActivity.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)), "articulos.json")
+
+                // Verificar si el archivo ya existe
+                if (targetFile.exists()) {
+                    // Si el archivo existe, eliminarlo para sobrescribirlo
+                    targetFile.delete()
+                    Log.d("FileCheck", "El archivo ${targetFile.name} ya existía, se ha eliminado para sobrescribirlo.")
+                }
+
+                copyFile(uri, targetFile)
+            }else if(uri.toString().contains("cbarras", ignoreCase = true)){
+
+                // Definir el archivo de destino
+                targetFile = File((this@LeerFicherosActivity.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)), "cbarras.json")
+
+                // Verificar si el archivo ya existe
+                if (targetFile.exists()) {
+                    // Si el archivo existe, eliminarlo para sobrescribirlo
+                    targetFile.delete()
+                    Log.d("FileCheck", "El archivo ${targetFile.name} ya existía, se ha eliminado para sobrescribirlo.")
+                }
+                copyFile(uri, targetFile)
             }
-            // Aquí puedes realizar cualquier acción posterior, como leer el archivo copiado
-            Toast.makeText(this, "Archivo ${destinationFile.name} copiado a: ${destinationFile.absolutePath}",
-                Toast.LENGTH_LONG).show()
-            Log.d("FileCopy", "Archivo copiado a: ${destinationFile.absolutePath}")
-
-
-        } catch (e: IOException) {
-            Toast.makeText(this, e.message.toString(),
-                Toast.LENGTH_LONG).show()
-            Log.d("FileCopy", e.message.toString())
-        }
-    } */
-
-    // Este método manejará el resultado del archivo seleccionado
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == PICK_JSON_FILE && resultCode == RESULT_OK) {
-            val uri = data?.data ?: return
-
-            // Definir el archivo de destino
-            val targetFile = File((this@LeerFicherosActivity.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)), "pepe.json")
-
-            // Verificar si el archivo ya existe
-            if (targetFile.exists()) {
-                // Si el archivo existe, eliminarlo para sobrescribirlo
-                targetFile.delete()
-                Log.d("FileCheck", "El archivo pepe.json ya existía, se ha eliminado para sobrescribirlo.")
-            }
-
-            // Copiar el archivo (esto creará el archivo si no existe, o lo sobrescribirá si ya existía)
-            copyFile(uri, targetFile)
         }
     }
 
@@ -139,6 +114,4 @@ class LeerFicherosActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
-
-
 }
